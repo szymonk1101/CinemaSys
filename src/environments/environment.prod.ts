3c0769de2c59690fd416c73439228406f8704b2c
Szymon Karolczuk <szymonk1101@gmail.com>
2020-12-30 16:52:17 +0100

    missing production environment's property

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://194.59.141.155:7777' // without slash on end!
+  apiUrl: 'http://194.59.141.155:7777', // without slash on end!
+  fileUrl: 'http://194.59.141.155:7777/file/'
 };
